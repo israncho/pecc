457d27144a8bf1b586ca77adaeeb5fba1ab2b217
Jesus Israel Gutierrez Elizalde <isragutierrez@ciencias.unam.mx>
2025-03-19 14:17:13 -0600

    better testing

--- include/test/utils/test_input_output.h
+++ include/test/utils/test_input_output.h
@@ -1,6 +1,36 @@
 #ifndef TEST_INPUT_OUTPUT_H
 #define TEST_INPUT_OUTPUT_H
 
+#include "../../utils/input_output.h"
+#include <stdbool.h>
+#include <stddef.h>
+
+/**
+ * @brief Tests the read_file function to verify its correctness.
+ *
+ * This function performs a series of test cases on the read_file function,
+ * checking whether it correctly reads a file into an array of file_line structures.
+ * The results of the test are displayed on the standard output.
+ */
 void test_read_file();
+
+/**
+ * @brief Compares two arrays of file_line structures for equality.
+ *
+ * This function checks if two arrays of file_line structures are equal in terms
+ * of size and content. It compares both the number of elements in the arrays and
+ * the content of each line.
+ *
+ * @param array_of_lines1 Pointer to the first array of file_line structures.
+ * @param arr1_size The number of elements in the first array.
+ * @param array_of_lines2 Pointer to the second array of file_line structures.
+ * @param arr2_size The number of elements in the second array.
+ * @return true if both arrays are identical in size and content, false otherwise.
+ */
+bool equality_test_for_line_arrays(file_line *array_of_lines1,
+                                   size_t arr1_size,
+                                   file_line *array_of_lines2,
+                                   size_t arr2_size);
+
 
 #endif
--- test/utils/test_input_output.c
+++ test/utils/test_input_output.c
@@ -1,8 +1,9 @@
 #include "../../include/test/utils/test_input_output.h"
 #include "../../include/utils/input_output.h"
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <string.h>
 
 void test_read_file() {
@@ -21,19 +22,51 @@ void test_read_file() {
                          {"dddddddddd\n", 11},
                          {"\n", 1},
                          {"\n", 1},
-                         {"eeeeeeeeee", 10}
-                    };
+                         {"eeeeeeeeee", 10}};
 
-  assert(*ptr_to_num_of_lines == 10);
-  for (size_t i = 0; i < *ptr_to_num_of_lines; i++) {
-    assert(file1[i].length == array_of_lines[i].length);
-    assert(array_of_lines[i].content[array_of_lines[i].length] == '\0');
-    assert(file1[i].content[file1[i].length] == '\0');
-    assert(strcmp(file1[i].content, array_of_lines[i].content) == 0);
+  bool test_result = equality_test_for_line_arrays(
+      array_of_lines, *ptr_to_num_of_lines, file1, 10);
+  assert(test_result);
+
+  for (size_t i = 0; i < *ptr_to_num_of_lines; i++)
     free_file_line(&array_of_lines[i]);
-  }
-  free(array_of_lines);
-  free(ptr_to_num_of_lines);
 
+  free(array_of_lines);
+  array_of_lines = NULL;
+  free(ptr_to_num_of_lines);
+  ptr_to_num_of_lines = NULL;
   printf("- read_file: PASSED\n");
+}
+
+bool equality_test_for_line_arrays(file_line *array_of_lines1, size_t arr1_size,
+                                   file_line *array_of_lines2,
+                                   size_t arr2_size) {
+  if (arr1_size != arr2_size) {
+    printf("arr1_size: %zu != arr2_size %zu\n", arr1_size, arr2_size);
+    return false;
+  }
+
+  for (size_t i = 0; i < arr1_size; i++) {
+    if (array_of_lines1[i].length != array_of_lines2[i].length) {
+      printf("array_of_lines1[%zu].length: %zu != array_of_lines2[%zu].length: "
+             "%zu\n",
+             i, array_of_lines1[i].length, i, array_of_lines2[i].length);
+      return false;
+    }
+    if (array_of_lines1[i].content[array_of_lines1[i].length] != '\0') {
+      printf("array_of_lines1[%zu].content[arr1_size] != \'\\0\'\n", i);
+      return false;
+    }
+    if (array_of_lines2[i].content[array_of_lines2[i].length] != '\0') {
+      printf("array_of_lines2[%zu].content[arr1_size] != \'\\0\'\n", i);
+      return false;
+    }
+    if (strcmp(array_of_lines1[i].content, array_of_lines2[i].content) != 0) {
+      printf("%s != %s", array_of_lines1[i].content,
+             array_of_lines2[i].content);
+      return false;
+    }
+  }
+
+  return true;
 }
