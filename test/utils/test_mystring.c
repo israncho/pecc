c9fbfb41bb2b6749faa3f026270d794920547792
Jesus Israel Gutierrez Elizalde <isragutierrez@ciencias.unam.mx>
2025-04-06 01:30:07 -0600

    better testing for split_in_place

--- test/utils/test_mystring.c
+++ test/utils/test_mystring.c
@@ -248,174 +248,114 @@ void test_split_to_buffer() {
   printf("\t- split_to_buffer: PASSED\n");
 }
 
+static void verify_split_in_place_operation(
+    const char *target[], const char *str, const size_t str_len,
+    const char delimiter, char ***ptr_to_tokens_array,
+    const size_t array_of_tokens_initial_capacity, const size_t expected_tokens,
+    char **ptr_to_buffer) {
+  assert(target != NULL);
+  assert(str != NULL);
+  assert(ptr_to_tokens_array != NULL);
+  assert(*ptr_to_tokens_array != NULL);
+  assert(ptr_to_buffer != NULL);
+  assert(*ptr_to_buffer != NULL);
+
+  assert(resize_array((void **)ptr_to_buffer, str_len + 1, sizeof(char)) ==
+         ARRAY_OK);
+  assert(resize_array((void **)ptr_to_tokens_array,
+                      array_of_tokens_initial_capacity,
+                      sizeof(char *)) == ARRAY_OK);
+
+  strcpy(*ptr_to_buffer, str);
+  size_t tokens_array_capacity = array_of_tokens_initial_capacity;
+  size_t num_of_tokens = 0;
+  assert(split_in_place(ptr_to_buffer, str_len, delimiter, ptr_to_tokens_array,
+                        &tokens_array_capacity, &num_of_tokens) == 0);
+  char **tokens_array = *ptr_to_tokens_array;
+  assert(num_of_tokens == expected_tokens);
+  assert(tokens_array_capacity >= num_of_tokens);
+  for (size_t i = 0; i < expected_tokens; i++)
+    assert(strcmp(target[i], tokens_array[i]) == 0);
+}
+
 void test_split_in_place() {
   size_t char_size = sizeof(char);
   size_t ptr_to_char_size = sizeof(char *);
-
   char *buffer = NULL;
-  size_t buffer_capacity = 40;
-  assert(init_array((void **)&buffer, buffer_capacity, char_size) == ARRAY_OK);
+  assert(init_array((void **)&buffer, 1, char_size) == ARRAY_OK);
 
   char **array_of_tokens = NULL;
   size_t array_of_tokens_capacity = 1;
   assert(init_array((void **)&array_of_tokens, array_of_tokens_capacity,
                     ptr_to_char_size) == ARRAY_OK);
 
-  size_t number_of_tokens = 0;
 
   // subtest 1
   char *test_split[] = {"hola", "amigo", "como", "estas"};
-  strcpy(buffer, "hola amigo como estas");
-  assert(split_in_place(&buffer, 21, ' ', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 4);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  for (size_t i = 0; i < number_of_tokens; i++)
-    assert(strcmp(test_split[i], array_of_tokens[i]) == 0);
+  verify_split_in_place_operation((const char **)test_split,
+                                  "hola amigo como estas", 21, ' ',
+                                  &array_of_tokens, 1, 4, &buffer);
 
-  // subtest 1.2
-  strcpy(buffer, "   hola   amigo   como    estas   ");
-  assert(split_in_place(&buffer, 34, ' ', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 4);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  for (size_t i = 0; i < number_of_tokens; i++)
-    assert(strcmp(test_split[i], array_of_tokens[i]) == 0);
+  verify_split_in_place_operation((const char **)test_split,
+                                  "   hola   amigo   como    estas   ", 34, ' ',
+                                  &array_of_tokens, 2, 4, &buffer);
 
-  // subtest 1.3
-  strcpy(buffer, "hola      amigo como      estas   ");
-  assert(split_in_place(&buffer, 34, ' ', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 4);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  for (size_t i = 0; i < number_of_tokens; i++)
-    assert(strcmp(test_split[i], array_of_tokens[i]) == 0);
+  verify_split_in_place_operation((const char **)test_split,
+                                  "hola      amigo como      estas   ", 34, ' ',
+                                  &array_of_tokens, 3, 4, &buffer);
+
+  verify_split_in_place_operation((const char **)test_split,
+                                  "          hola    amigo como estas", 34, ' ',
+                                  &array_of_tokens, 8, 4, &buffer);
 
   // subtest 2
   char *test_split2[] = {"a"};
-  assert(resize_array((void **)&array_of_tokens, 1, ptr_to_char_size) ==
-         ARRAY_OK);
-  array_of_tokens_capacity = 1;
+  verify_split_in_place_operation((const char **)test_split2, "a", 1, ' ',
+                                  &array_of_tokens, 1, 1, &buffer);
 
-  strcpy(buffer, "a");
-  assert(split_in_place(&buffer, 1, ' ', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 1);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  assert(strcmp(test_split2[0], array_of_tokens[0]) == 0);
+  verify_split_in_place_operation((const char **)test_split2, "   a", 4, ' ',
+                                  &array_of_tokens, 2, 1, &buffer);
 
-  // subtest 2.2
-  strcpy(buffer, "   a");
-  assert(split_in_place(&buffer, 4, ' ', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 1);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  assert(strcmp(test_split2[0], array_of_tokens[0]) == 0);
+  verify_split_in_place_operation((const char **)test_split2, "a   ", 4, ' ',
+                                  &array_of_tokens, 3, 1, &buffer);
 
-  // subtest 2.3
-  strcpy(buffer, "   a");
-  assert(split_in_place(&buffer, 4, ' ', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 1);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  assert(strcmp(test_split2[0], array_of_tokens[0]) == 0);
+  verify_split_in_place_operation((const char **)test_split2, "  a ", 4, ' ',
+                                  &array_of_tokens, 4, 1, &buffer);
 
-  // subtest 2.4
-  strcpy(buffer, "  a ");
-  assert(split_in_place(&buffer, 4, ' ', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 1);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  assert(strcmp(test_split2[0], array_of_tokens[0]) == 0);
-
-  // subtest 2.5
-  strcpy(buffer, "a");
-  assert(split_in_place(&buffer, 1, ',', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 1);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  assert(strcmp(test_split2[0], array_of_tokens[0]) == 0);
+  verify_split_in_place_operation((const char **)test_split2, "a", 1, ',',
+                                  &array_of_tokens, 1, 1, &buffer);
 
   // subtest 3
   char *test_split3[] = {"1", "2", "3", "4"};
-  assert(resize_array((void **)&array_of_tokens, 1, ptr_to_char_size) ==
-         ARRAY_OK);
-  array_of_tokens_capacity = 1;
+  verify_split_in_place_operation((const char **)test_split3, "1,2,3,4", 7, ',',
+                                  &array_of_tokens, 1, 4, &buffer);
 
-  strcpy(buffer, "1,2,3,4");
-  assert(split_in_place(&buffer, 7, ',', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 4);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  for (size_t i = 0; i < 4; i++)
-    assert(strcmp(test_split3[i], array_of_tokens[i]) == 0);
+  verify_split_in_place_operation((const char **)test_split3, ",,,1,,2,3,,4,,,",
+                                  15, ',', &array_of_tokens, 4, 4, &buffer);
 
-  // subtest 3.1
-  strcpy(buffer, ",,,1,2,3,4,,,");
-  assert(split_in_place(&buffer, 13, ',', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 4);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  for (size_t i = 0; i < 4; i++)
-    assert(strcmp(test_split3[i], array_of_tokens[i]) == 0);
+  verify_split_in_place_operation((const char **)test_split3, "1-2-3-4", 7, '-',
+                                  &array_of_tokens, 5, 4, &buffer);
 
-  // subtest 3.2
-  strcpy(buffer, "1-2-3-4");
-  assert(split_in_place(&buffer, 7, '-', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 4);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  for (size_t i = 0; i < 4; i++)
-    assert(strcmp(test_split3[i], array_of_tokens[i]) == 0);
-
-  // subtest 3.3
-  strcpy(buffer, "1\n2\n3\n4");
-  assert(split_in_place(&buffer, 7, '\n', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-  assert(number_of_tokens == 4);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  for (size_t i = 0; i < 4; i++)
-    assert(strcmp(test_split3[i], array_of_tokens[i]) == 0);
+  verify_split_in_place_operation((const char **)test_split3, "1\n2\n3\n4", 7,
+                                  '\n', &array_of_tokens, 1, 4, &buffer);
 
   // subtest 4
   char *test_split4[] = {"hola_amigo_como_estas"};
-  assert(resize_array((void **)&array_of_tokens, 1, ptr_to_char_size) ==
-         ARRAY_OK);
-  array_of_tokens_capacity = 1;
+  verify_split_in_place_operation((const char **)test_split4,
+                                  "hola_amigo_como_estas", 21, ',',
+                                  &array_of_tokens, 1, 1, &buffer);
 
-  strcpy(buffer, "hola_amigo_como_estas");
-  assert(split_in_place(&buffer, 21, ',', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
+  verify_split_in_place_operation((const char **)test_split4,
+                                  "hola_amigo_como_estas", 21, ' ',
+                                  &array_of_tokens, 3, 1, &buffer);
 
-  assert(number_of_tokens == 1);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  assert(strcmp(test_split4[0], array_of_tokens[0]) == 0);
+  verify_split_in_place_operation((const char **)test_split4,
+                                  ",hola_amigo_como_estas", 22, ',',
+                                  &array_of_tokens, 1, 1, &buffer);
 
-  // subtest 4.1
-  assert(split_in_place(&buffer, 21, ' ', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-
-  assert(number_of_tokens == 1);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  assert(strcmp(test_split4[0], array_of_tokens[0]) == 0);
-
-  // subtest 4.2
-  strcpy(buffer, ",hola_amigo_como_estas");
-  assert(split_in_place(&buffer, 22, ',', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-
-  assert(number_of_tokens == 1);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  assert(strcmp(test_split4[0], array_of_tokens[0]) == 0);
-
-  // subtest 4.2
-  strcpy(buffer, "hola_amigo_como_estas,");
-  assert(split_in_place(&buffer, 22, ',', &array_of_tokens,
-                        &array_of_tokens_capacity, &number_of_tokens) == 0);
-
-  assert(number_of_tokens == 1);
-  assert(array_of_tokens_capacity >= number_of_tokens);
-  assert(strcmp(test_split4[0], array_of_tokens[0]) == 0);
+  verify_split_in_place_operation((const char **)test_split4,
+                                  "hola_amigo_como_estas,", 22, ',',
+                                  &array_of_tokens, 5, 1, &buffer);
 
   free(buffer);
   free(array_of_tokens);
