b3abded9ae661f1e43a45338d6ce71a7e083cdf0
Jesus Israel Gutierrez Elizalde <isragutierrez@ciencias.unam.mx>
2025-04-10 20:34:01 -0600

    small details

--- include/evo_comp/genetic_algorithm.h
+++ include/evo_comp/genetic_algorithm.h
@@ -4,6 +4,7 @@
 #define GENETIC_ALGORITHM_H
 
 #include <stddef.h>
+#include "../../include/utils/myrandom.h"
 
 typedef struct {
   void *codification;
@@ -15,8 +16,21 @@ typedef struct {
   size_t generations;
   size_t total_f_execs;
   size_t curr_f_execs;
+  size_t n_threads;
   individual current_best;
+  xorshiftr128plus_state *random_state;
 } ga_execution;
+
+typedef struct {
+  void *crossover_workspace;
+  void *selection_workspace;
+  void *mutation_workspace;
+  void *replacement_workspace;
+  size_t crossover_workspace_capacity;
+  size_t selection_workspace_capacity;
+  size_t mutation_workspace_capacity;
+  size_t replacement_workspace_capacity;
+} ga_workspace;
 
 int genetic_algorith(individual *array_of_individuals,
                      individual *array_for_offpring,
