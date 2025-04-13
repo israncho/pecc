af1c909124700d023246eee22aa1f6a8e8e9040d
Jesus Israel Gutierrez Elizalde <isragutierrez@ciencias.unam.mx>
2025-04-13 16:07:38 -0600

    now, true high performance random numbers

--- include/utils/myrandom.h
+++ include/utils/myrandom.h
@@ -26,7 +26,14 @@ static inline uint64_t xorshiftr128plus(xorshiftr128plus_state *state) {
 }
 
 static inline size_t randsize_t(size_t max, xorshiftr128plus_state *state) {
-  return xorshiftr128plus(state) % max;
+  uint64_t x = xorshiftr128plus(state);
+  return ((__uint128_t)x * (__uint128_t)max) >> 64;
+}
+
+static inline size_t randsize_t_i(size_t min, size_t max, xorshiftr128plus_state *state) {
+  size_t interval_size = max - min + 1;
+  uint64_t x = xorshiftr128plus(state);
+  return (((__uint128_t)x * (__uint128_t)interval_size) >> 64) + min;
 }
 
 int shuffle_array_of_size_t(size_t *array, const size_t array_size, xorshiftr128plus_state *state);
